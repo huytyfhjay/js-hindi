350dc67572b52d05f418e854311ed717cc9d7397
JAY DINESH YADAV <156833128+huytyfhjay@users.noreply.github.com>
2026-06-25 13:26:36 +0000

    yes check

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+"use strict" // Treat all js code as a newer version 
+
+//alert( 3+ 3 ) // we are using nodejs, not browser 
+
+console.log(3
+    +3       // this not redeable code 
+      )
+
+
+console.log("jay Yadav")
