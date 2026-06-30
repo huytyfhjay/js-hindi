d84b0d7044d96e3aa5bda6ed8f432168c1b27139
JAY DINESH YADAV <156833128+huytyfhjay@users.noreply.github.com>
2026-06-30 08:24:13 +0000

    datatypes

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,10 +1,29 @@
-"use strict" // Treat all js code as a newer version 
+"use strict"; // treat all JS code as newer version
 
-//alert( 3+ 3 ) // we are using nodejs, not browser 
+// alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3
-    +3       // this not redeable code 
-      )
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
 
 
-console.log("jay Yadav")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
